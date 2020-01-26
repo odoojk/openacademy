e6b88837c5b713c9ac47eb87d045a7d9aec6c320
jk <odoo.jk@gmail.com>
2020-01-26 20:38:59 +0700

    init openacademy

---
+++ models/models.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class openacademy(models.Model):
+#     _name = 'openacademy.openacademy'
+#     _description = 'openacademy.openacademy'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+
+
+from odoo import models, fields, api
+
+class Course(models.Model):
+    _name = 'openacademy.course'
+    _description = "OpenAcademy Courses"
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+
+class Session(models.Model):
+    _name = 'openacademy.session'
+    _description = "OpenAcademy Sessions"
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
