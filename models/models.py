38bfc0170b7fbe85a7e294212d52669bd1dd3945
jk <odoo.jk@gmail.com>
2020-01-26 22:10:39 +0700

    session

--- models/models.py
+++ models/models.py
@@ -27,6 +27,10 @@ class Course(models.Model):
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
 
+    responsible_id = fields.Many2one('res.users',
+        ondelete='set null', string="Responsible", index=True)
+
+
 class Session(models.Model):
     _name = 'openacademy.session'
     _description = "OpenAcademy Sessions"
@@ -35,3 +39,13 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('openacademy.course',
+        ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
+#odoo.jk
+# odoojakkrit
+# odoojk
+# odoojk9876543210
